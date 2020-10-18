e22767836cb9d67685e656e1f86f87835510a1ad
Robert Hanck <hanckrobert@hotmail.com>
2020-10-18 15:47:35 -0300

    Cadastrar lead.

--- src/app/services/lead/lead.service.ts
+++ src/app/services/lead/lead.service.ts
@@ -1,16 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiResponse } from 'src/app/models/api-response';
+import { Lead } from 'src/app/models/lead';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeadService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getLeads() {
     return this.http.get<ApiResponse>(`${environment.baseApiUrl}/leads`);
+  }
+
+  updateStatus(lead: Lead) {
+    return this.http.patch<ApiResponse | any>(`${environment.baseApiUrl}/leads/${lead.id}`, {
+      id: lead.id,
+      statusId: lead.statusId,
+    });
+  }
+
+  save(lead: Lead) {
+    return this.http.post<ApiResponse>(`${environment.baseApiUrl}/leads`, lead);
   }
 }
